4b8aaa4b11c36bbf7a494b89035489e9a6892c91
TiagoLourinho <tiagolourinho001@gmail.com>
2022-11-26 13:38:43 +0000

    finding contours

--- Lab1/extract_points.py
+++ Lab1/extract_points.py
@@ -1,20 +1,25 @@
 import numpy as np
-import cv2
+import cv2 as cv
+import os
+
+IMAGE_PATH = "Lab1/images/house.png"
 
 
-pixel_to_mm = 0.2645833333
+img = cv.imread(os.path.normpath(IMAGE_PATH))
+img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+contours, hierarchy = cv.findContours(
+    cv.bitwise_not(img),
+    cv.RETR_TREE,
+    cv.CHAIN_APPROX_SIMPLE,
+)
+
+blank_image = 255 * np.ones_like(img)
+cv.drawContours(blank_image, contours, -1, 0, 1)
+
+cv.imshow("Original image", img)
+cv.imshow(f"Contours found ({len(contours)})", blank_image)
 
 
-def create_point_list(img):
-    pass
-
-
-img = cv2.imread("Lab1/images/house.png")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-
-cv2.imshow("image", img)
-
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv.waitKey(0)
+cv.destroyAllWindows()
