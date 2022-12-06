c131500408c6bc1fc822769d42d2576df365a843
TiagoWebMaster <tiagombferreira@hotmail.com>
2022-12-06 16:16:06 +0000

    added 'drawing'

--- Lab1/src/simulator.py
+++ Lab1/src/simulator.py
@@ -1,7 +1,3 @@
-#import numpy as np
-#import matplotlib.pyplot as plt
-
-
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -9,25 +5,8 @@ from matplotlib import cm
 from matplotlib.widgets import Slider, Button
 
 
+draw_points = np.array([0, 0 ,0, 1])
 
-V = np.array([[0 ,0.2,0.28752689], [0, 0.2,0.2236], [0, 0.287228,0.2],[0, 0.1,0.23]])
-
-points = np.array([[0,0,0]])
-for i,vector in enumerate(V): points = np.append(points,[points[i]+vector],axis=0)
-
-def angle_dot(a, b):
-    dot_product = np.dot(a, b)
-    prod_of_norms = np.linalg.norm(a) * np.linalg.norm(b)
-    return np.arccos(dot_product / prod_of_norms)
-
-
-
-
-alpha = angle_dot([0,1,0],V[0])
-beta = angle_dot(V[0],V[1])
-gamma = angle_dot(V[1],V[2])
-delta = angle_dot(V[2],V[3])
-print(alpha,beta,gamma,delta)
 
 fig, ax = plt.subplots(1, 1)
 ax = plt.axes(projection='3d')
@@ -40,43 +19,51 @@ ax.set_ylabel('Y axis')
 ax.set_zlabel('Z axis')
 plt.subplots_adjust(left=0.25, bottom=0.4)
 plt.grid(visible=True)
-l,=plt.plot(points[:,0], points[:,1],'.',linestyle='-', linewidth=2, markersize=12)
-
+l,=plt.plot([0], [0],'.',linestyle='-', linewidth=2, markersize=12)
+l2,=plt.plot([0], [0], [0], markerfacecolor='k', markeredgecolor='k' ,marker='o', markersize=2, alpha=0.6,linestyle = 'None')
 
 axbase = plt.axes([0.25, 0.3, 0.65, 0.03])
 axa = plt.axes([0.25, 0.25, 0.65, 0.03])
 axb  = plt.axes([0.25, 0.2, 0.65, 0.03])
 axc = plt.axes([0.25, 0.15, 0.65, 0.03])
-axd  = plt.axes([0.25, 0.1, 0.65, 0.03])
 
 
 base= Slider(axbase, 'base', 0.0 , 2*np.pi, valinit=0)
-alpha_= Slider(axa, 'alpha', -np.pi/2 , np.pi/2, valinit=alpha)
-beta = Slider(axb, 'beta',-np.pi/2 , np.pi/2, valinit=beta)
-gamma = Slider(axc, 'gamma', 0.00, 2*np.pi, valinit=gamma)
-delta = Slider(axd, 'delta', 0.00, 2*np.pi, valinit=delta)
+alpha_= Slider(axa, 'alpha', -np.pi/2 , np.pi/2, valinit=0)
+beta = Slider(axb, 'beta',-np.pi/2 , np.pi/2, valinit=0)
+gamma = Slider(axc, 'gamma', 0.00, 2*np.pi, valinit=0)
 def update(val):
+    global draw_points 
+    #matrizes de transformação
     alpha,a,d,teta = (0,0,0.3585,base.val)
-    t01 = np.array([[np.cos(teta), -np.sin(teta), 0,a],[np.cos(alpha)*np.sin(teta), np.cos(alpha)*np.cos(teta), -np.sin(alpha),-np.sin(alpha)*d],[np.sin(alpha)*np.sin(teta), np.sin(alpha)*np.cos(teta),np.cos(alpha),np.cos(alpha)*d],[0, 0, 0,1]])
+    t10 = np.array([[np.cos(teta), -np.sin(teta), 0,a],[np.cos(alpha)*np.sin(teta), np.cos(alpha)*np.cos(teta), -np.sin(alpha),-np.sin(alpha)*d],[np.sin(alpha)*np.sin(teta), np.sin(alpha)*np.cos(teta),np.cos(alpha),np.cos(alpha)*d],[0, 0, 0,1]])
     alpha,a,d,teta = (np.pi/2+alpha_.val,0,0.300,0)
-    t12= np.array([[np.cos(teta), -np.sin(teta), 0,a],[np.cos(alpha)*np.sin(teta), np.cos(alpha)*np.cos(teta), -np.sin(alpha),-np.sin(alpha)*d],[np.sin(alpha)*np.sin(teta), np.sin(alpha)*np.cos(teta),np.cos(alpha),np.cos(alpha)*d],[0, 0, 0,1]])
+    t21= np.array([[np.cos(teta), -np.sin(teta), 0,a],[np.cos(alpha)*np.sin(teta), np.cos(alpha)*np.cos(teta), -np.sin(alpha),-np.sin(alpha)*d],[np.sin(alpha)*np.sin(teta), np.sin(alpha)*np.cos(teta),np.cos(alpha),np.cos(alpha)*d],[0, 0, 0,1]])
     alpha,a,d,teta = (beta.val,0,0.350,0)
-    t23 = np.array([[np.cos(teta), -np.sin(teta), 0,a],[np.cos(alpha)*np.sin(teta), np.cos(alpha)*np.cos(teta), -np.sin(alpha),-np.sin(alpha)*d],[np.sin(alpha)*np.sin(teta), np.sin(alpha)*np.cos(teta),np.cos(alpha),np.cos(alpha)*d],[0, 0, 0,1]])
+    t32 = np.array([[np.cos(teta), -np.sin(teta), 0,a],[np.cos(alpha)*np.sin(teta), np.cos(alpha)*np.cos(teta), -np.sin(alpha),-np.sin(alpha)*d],[np.sin(alpha)*np.sin(teta), np.sin(alpha)*np.cos(teta),np.cos(alpha),np.cos(alpha)*d],[0, 0, 0,1]])
     alpha,a,d,teta = (gamma.val,0,0.251,0)
-    t34 = np.array([[np.cos(teta), -np.sin(teta), 0,a],[np.cos(alpha)*np.sin(teta), np.cos(alpha)*np.cos(teta), -np.sin(alpha),-np.sin(alpha)*d],[np.sin(alpha)*np.sin(teta), np.sin(alpha)*np.cos(teta),np.cos(alpha),np.cos(alpha)*d],[0, 0, 0,1]])
-    transformations = np.array([t01,t12,t23,t34])
+    t43 = np.array([[np.cos(teta), -np.sin(teta), 0,a],[np.cos(alpha)*np.sin(teta), np.cos(alpha)*np.cos(teta), -np.sin(alpha),-np.sin(alpha)*d],[np.sin(alpha)*np.sin(teta), np.sin(alpha)*np.cos(teta),np.cos(alpha),np.cos(alpha)*d],[0, 0, 0,1]])
+    
     points = np.array([[0,0,0,1]])
-    
-    for i,transf in enumerate(transformations): 
-        print(points)
-        points = np.append(points,[np.dot(transf,points[i])],axis=0)
+    t20 = np.matmul(t10,t21)
+    t30 = np.matmul(t20,t32)
+    t40 = np.matmul(t30,t43)
 
-    alpha,a,d,teta = (0,0,0.0,base.val)
-    t01 = np.array([[np.cos(teta), -np.sin(teta), 0,a],[np.cos(alpha)*np.sin(teta), np.cos(alpha)*np.cos(teta), -np.sin(alpha),-np.sin(alpha)*d],[np.sin(alpha)*np.sin(teta), np.sin(alpha)*np.cos(teta),np.cos(alpha),np.cos(alpha)*d],[0, 0, 0,1]])
+    points = np.vstack((points,t10.dot(np.array([0,0,0,1]))))
+    points = np.vstack((points,t21.dot(np.array([0,0,0,1]))))
+    points = np.vstack((points,t32.dot(np.array([0,0,0,1]))))
+    points = np.vstack((points,t43.dot(np.array([0,0,0,1]))))
+
     
-    points = t01.dot(points.T)
-    points = points.T
+    points[2] = np.matmul((t20),np.array([0,0,0,1]))
+    points[3] = np.matmul((t30),np.array([0,0,0,1]))
+    points[4] = np.matmul((t40),np.array([0,0,0,1]))
     print(points)
+    if ((points[4][2] < 0.01) and (points[4][2] > -0.01)):
+        draw_points = np.vstack((draw_points,points[4]))
+        l2.set_xdata(draw_points[:,0])
+        l2.set_ydata(draw_points[:,1])
+        l2.set_3d_properties([0])
     l.set_xdata(points[:,0])
     l.set_ydata(points[:,1])
     l.set_3d_properties(points[:,2])
@@ -86,5 +73,4 @@ base.on_changed(update)
 alpha_.on_changed(update)
 beta.on_changed(update)
 gamma.on_changed(update)
-delta.on_changed(update)
 plt.show()
